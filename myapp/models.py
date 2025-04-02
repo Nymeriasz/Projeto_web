8020d77f02f2b98868cadc13f7ff461fe39fdbbb
Pâmella Maria <pamellamariadev@gmail.com>
2025-04-01 23:55:31 -0300

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from myapp.base_model import BaseModel, ActiveManager
 
-# Create your models here.
+class Product(BaseModel):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    objects = ActiveManager() 
+    all_objects = models.Manager()  
+
+    def __str__(self):
+        return self.name
